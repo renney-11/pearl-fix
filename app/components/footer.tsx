da82eea60ed6325d5c0112f6b1f73763ac916976
labbaci <labbaci@student.chalmers.se>
2024-11-15 17:57:17 +0100

    #5 create footer component Co-authored-by: Manely Abbasi <manely@student.chalmers.se>

---
+++ app/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-blue-900 py-6 px-8 fixed inset-x-0 bottom-0">
+      <div className="container mx-auto flex items-center justify-center">
+        <p className="text-white text-center">
+          © 2024 Copyright: toothbeacon.com
+        </p>
+      </div>
+    </footer>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Other content goes here */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
