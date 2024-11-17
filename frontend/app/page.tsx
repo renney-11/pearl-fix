5339ce002a89e3fe0717363c601a3368b3745b9d
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2024-11-17 13:28:52 +0100

    restrucutre system

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from '../src/components/header';
+import Footer from '../src/components/footer';
 
 export default function App() {
   return (
