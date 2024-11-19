6920024755e0ab118bf6d5554659ebdea30bcb74
labbaci <labbaci@student.chalmers.se>
2024-11-19 16:37:25 +0100

    #5 update subbackground margin

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/src/components/footer';
 import Header from "@/src/components/header";
 import Background from '@/src/components/background';
+import SubBackground from '@/src/components/subbackground';
 
 
 export default function App() {
@@ -8,9 +9,11 @@ export default function App() {
     <div>
       <Background>
       <Header />
+      <SubBackground>
       <main>
         {/* Other content goes here */}
       </main>
+      </SubBackground>
       </Background>
       <Footer />
     </div>
