e74be7715b1c6497796c6ed4e1ad2672e8bbccc9
manely <manely@student.chalmers.se>
2024-11-19 16:32:24 +0100

    #5 update page.tsx to include background Co-authored-by: Celina Labbaci <labbaci@student.chalmers.se>

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,17 @@
 import Footer from '@/src/components/footer';
 import Header from "@/src/components/header";
+import Background from '@/src/components/background';
 
 
 export default function App() {
   return (
     <div>
+      <Background>
       <Header />
       <main>
         {/* Other content goes here */}
       </main>
+      </Background>
       <Footer />
     </div>
   );
