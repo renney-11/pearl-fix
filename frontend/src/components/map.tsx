14c6505b0cd368b71239b212574c3da3bf876514
renaa <renaa@chalmers.se>
2024-11-21 15:11:35 +0100

    #10 add customize icon

--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -1,20 +1,27 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+"use client";
+import { faStreetView } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { divIcon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
-// A custom component to update the map view to the user's location dynamically
-const UpdateMapView: React.FC<{ location: [number, number] }> = ({ location }) => {
+// Custom Map View Updater
+const UpdateMapView: React.FC<{ location: [number, number] }> = ({
+  location,
+}) => {
   const map = useMap();
   useEffect(() => {
     map.setView(location, 13);
   }, [location, map]);
-
   return null;
 };
 
 const Map: React.FC = () => {
-  const [userLocation, setUserLocation] = useState<[number, number] | null>(null);
+  const [userLocation, setUserLocation] = useState<[number, number] | null>(
+    null
+  );
 
   // Fetch the user's current location
   useEffect(() => {
@@ -22,7 +29,6 @@ const Map: React.FC = () => {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          console.log("User location:", { latitude, longitude });
           setUserLocation([latitude, longitude]);
         },
         (error) => {
@@ -38,13 +44,25 @@ const Map: React.FC = () => {
   // Fallback coordinates if user's location is not available
   const gothenburgCoords: [number, number] = [57.7089, 11.9746];
 
+  // Define custom marker icon
+  const customIcon = divIcon({
+    html: renderToStaticMarkup(
+      <div className="text-[24px] text-[#1e3582]">
+        <FontAwesomeIcon icon={faStreetView} />
+      </div>
+    ),
+    className: "custom-div-icon",
+    iconAnchor: [12, 24], // Anchor to ensure icon is properly centered
+    popupAnchor: [0, -24], // Ensure the popup is directly centered beneath the icon
+  });
+
   return (
     <div className="h-[400px] w-full">
       <MapContainer
         center={gothenburgCoords}
         zoom={13}
         scrollWheelZoom={true}
-        style={{ height: '100%', width: '100%' }}
+        className="h-full w-full"
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -52,8 +70,13 @@ const Map: React.FC = () => {
         />
         {userLocation && (
           <>
-            <Marker position={userLocation}>
-              <Popup>You are here!</Popup>
+            <Marker position={userLocation} icon={customIcon}>
+              <Popup closeButton={true} >
+                <div>
+                <span className="text-main-blue">You are here!</span>
+                <div className="absolute w-3 h-3 rotate-45 transform bottom-[-6px] left-1/2 translate-x-[-50%] z-10" />
+                </div>
+              </Popup>
             </Marker>
             <UpdateMapView location={userLocation} />
           </>
