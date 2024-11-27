ffef2ed847f3ab84c9b16649fd9ce8a2c7dfafc3
manely <manely@student.chalmers.se>
2024-11-27 14:08:03 +0100

    #17 add styling

--- frontend/app/patient-tool/landing-page/page.tsx
+++ frontend/app/patient-tool/landing-page/page.tsx
@@ -7,6 +7,7 @@ import Footer from "@/src/components/footer";
 import Header from "@/src/components/header";
 import Background from "@/src/components/background";
 
+
 interface Service {
   id: number;
   icon: any;
@@ -26,18 +27,23 @@ const PatientLandingPage: React.FC = () => {
   return (
     <Background>
       <Header />
-      <main>
-        <h2>select a service:</h2>
-        <div>
+      <main className="text-center p-8 min-h-screen flex flex-col items-center justify-start mt-12">
+        <h2 className="text-3xl font-bold text-[#1E3582] mb-20">select a service:</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-screen-xl px-8">
           {services.map((service) => (
             <a
               key={service.id}
               href={service.link}
+              className={`bg-[#1E3582] text-white rounded-2xl p-10 flex flex-col items-center justify-center 
+                shadow-lg hover:shadow-xl transition-transform transform ${
+                  hovered === service.id ? "translate-y-[-6px]" : "translate-y-0"
+                }`}
               onMouseEnter={() => setHovered(service.id)}
               onMouseLeave={() => setHovered(null)}
+              style={{ minHeight: "200px" }}
             >
-              <FontAwesomeIcon icon={service.icon} />
-              <p>{service.text}</p>
+              <FontAwesomeIcon icon={service.icon} className="text-white text-5xl mb-4" />
+              <p className="text-xl capitalize text-[#E0DCFB]">{service.text}</p>
             </a>
           ))}
         </div>
