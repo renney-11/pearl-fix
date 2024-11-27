6780bacda333dd889767df963b22352f4f5c1d70
manely <manely@student.chalmers.se>
2024-11-27 23:00:45 +0100

    #20 update login page design

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+import React from "react";
+
 export default function Login() {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-main-blue">
-        <div className="w-full max-w-lg p-8 bg-transparent-blue rounded-lg shadow-lg">
-          <div className="flex flex-col items-center mb-8">
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[rgba(3,0,45)]">
+      <div className="w-full max-w-2xl p-8 bg-[rgba(180,195,220,255)] rounded-lg shadow-lg">
+        <div className="flex flex-col items-center mb-8">
           <a href="/">
             <img
               src="/assets/logo_vertical.png"
@@ -10,34 +13,40 @@ export default function Login() {
               className="w-40 h-35 mb-4 mt-1"
             />
           </a>
-          </div>
-          <form className="space-y-4">
-
-            <input
-              type="email"
-              placeholder="email"
-              className="w-full px-4 py-2 rounded bg-transparent-input text-white placeholder-blue-300 focus:outline-none"
-            />
-            <input
-              type="password"
-              placeholder="password"
-              className="w-full px-4 py-2 rounded bg-transparent-input text-white placeholder-blue-300 focus:outline-none"
-            />
- 
-            <button
-              type="submit"
-               className="py-2 px-8 mt-4 text-blue-900 bg-white rounded-full font-semibold hover:bg-gray-200 mx-auto block"
-            >
-              login
-            </button>
-          </form>
-          <p className="mt-4 text-center text-white">
-          new to tooth beacon?{" "}
-            <a href="/register" className="font-semibold text-blue-300">
-            create an account
-            </a>
-          </p>
         </div>
+        <form className="space-y-4">
+          <div className="flex justify-between items-center gap-4">
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="email"
+              className="w-full px-4 py-2 rounded border border-main-blue bg-transparent placeholder-main-blue text-main-blue focus:outline-none focus:ring-2 focus:ring-main-blue"
+            />
+          </div>
+          <div className="flex justify-between items-center gap-4">
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="password"
+              className="w-full px-4 py-2 rounded border border-main-blue bg-transparent placeholder-main-blue text-main-blue focus:outline-none focus:ring-2 focus:ring-main-blue"
+            />
+          </div>
+          <button
+            type="submit"
+            className="py-2 px-8 mt-4 bg-main-blue text-white rounded-full font-semibold hover:bg-blue-900 mx-auto block"
+          >
+            login
+          </button>
+        </form>
+        <p className="mt-4 text-center text-main-blue">
+          new to tooth beacon?{" "}
+          <a href="/register" className="font-semibold text-main-blue">
+            create an account
+          </a>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
+}
