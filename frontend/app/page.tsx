0b9044c37ebb6e04f15e9d45cd608e70fe0adb75
manely <manely@student.chalmers.se>
2024-11-28 16:11:15 +0100

    #21 create bottom section of homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTooth } from "@fortawesome/free-solid-svg-icons"; 
 import Header from "@/src/components/header";
 import Background from "@/src/components/background";
 
@@ -16,7 +18,7 @@ export default function App() {
       <Background>
         <Header />
         <main>
-          <div className="flex flex-col items-center py-20" >
+          <div className="flex flex-col items-center py-20 mb-20">
             {/* Flex container for the SVG images with gap between them */}
             <div className="flex">
               <img
@@ -48,6 +50,53 @@ export default function App() {
             >
               read more about us below
             </button>
+          </div>
+
+          <div
+            id="description-section"
+            className="bg-transparent-blue py-32 px-8 mb-20 mx-auto flex"
+            style={{ backgroundColor: 'bg-transparent-blue' }}
+          >
+            <h2 className="text-4xl text-main-blue font-bold mb-20 mx-10 mt-16">
+              WITH TOOTH BEACON YOU CAN...
+            </h2>
+            <ul className="text-main-blue space-y-5 text-xl mx-10">
+              <li>
+                <FontAwesomeIcon
+                  icon={faTooth}
+                  style={{ color: "#ffffff", marginRight: "10px" }}
+                />
+                find dental clinics near you
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faTooth}
+                  style={{ color: "#ffffff", marginRight: "10px" }}
+                />
+                book appointments at your chosen dental clinic
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faTooth}
+                  style={{ color: "#ffffff", marginRight: "10px" }}
+                />
+                find appointments as soon as possible, most suitable
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faTooth} 
+                  style={{ color: "#ffffff", marginRight: "10px" }}
+                />
+                easily cancel your appointments
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faTooth} 
+                  style={{ color: "#ffffff", marginRight: "10px" }}
+                />
+                be notified with updates regarding appointments
+              </li>
+            </ul>
           </div>
         </main>
       </Background>
