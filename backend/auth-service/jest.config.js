84f212c191da0d507bdf842319c0d1286e6007a4
renaa <renaa@chalmers.se>
2024-12-09 14:57:27 +0100

    #29 update package.json to run jest test

--- backend/auth-service/jest.config.js
+++ backend/auth-service/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  };
+  preset: 'ts-jest',  // Use ts-jest preset for TypeScript
+  testEnvironment: 'node',  // Use Node.js environment for testing
+  roots: ['<rootDir>/tests'],  // Point Jest to your 'tests' folder (adjust if your tests are located elsewhere)
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',  // Use ts-jest to process .ts and .tsx files
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],  // Ignore these folders during testing
+};
