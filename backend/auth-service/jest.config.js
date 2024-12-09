a62654ec8b1792175f0a278439d493c5e62912d9
renaa <renaa@chalmers.se>
2024-12-09 15:26:18 +0100

    #29 correct tests path in jest.config.js

--- backend/auth-service/jest.config.js
+++ backend/auth-service/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',  // Use ts-jest preset for TypeScript
   testEnvironment: 'node',  // Use Node.js environment for testing
-  roots: ['<rootDir>/backend/auth-service/src/tests'],  // Point to the correct directory for tests
+  roots: ['<rootDir>/src/tests'],  // Point to the correct directory for tests
   transform: {
     '^.+\\.tsx?$': 'ts-jest',  // Use ts-jest to process .ts and .tsx files
   },
